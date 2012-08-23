e52673c88f66fff855571624f71e7748837dbcce
Juan Carlos Jimenez Flores <carlos.jf.1681@gmail.com>
2012-08-22 22:35:58 -0500

    Pequeño cambio

--- protected/views/layouts/column2.php
+++ protected/views/layouts/column2.php
@@ -1,5 +1,5 @@
 <?php $this->beginContent('//layouts/main'); ?>
-<div class="span-19">
+<div class="span-16">
 	<div id="content">
 		<?php echo $content; ?>
 	</div><!-- content -->
@@ -7,17 +7,13 @@
 <div class="span-5 last">
 	<div id="sidebar">
 	<?php
-		$this->beginWidget('zii.widgets.CPortlet', array(
-			'title'=>'Operations',
-		));
-		$this->widget('zii.widgets.CMenu', array(
-			'items'=>$this->menu,
-			'htmlOptions'=>array('class'=>'operations'),
-		));
-		$this->endWidget();
-	?>
 
-	
+		$this->widget('bootstrap.widgets.TbMenu', array(
+			'type'=>'list',
+			'items'=>$this->menu
+		));
+
+	?>
 	</div><!-- sidebar -->
 </div>
 <?php $this->endContent(); ?>
